b340de909086336f9245a2782e89d06b7441b7bc
Crowdbotics <team@crowdbotics.com>
2022-10-24 16:10:14 +0000

    Update models

--- users/migrations/0002_auto_20221024_1609.py
+++ users/migrations/0002_auto_20221024_1609.py
@@ -6,38 +6,38 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='city',
+            model_name="user",
+            name="city",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='lastname',
+            model_name="user",
+            name="lastname",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='state',
+            model_name="user",
+            name="state",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='streetaddress',
+            model_name="user",
+            name="streetaddress",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='zip',
+            model_name="user",
+            name="zip",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='name',
+            model_name="user",
+            name="name",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
